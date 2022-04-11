d937301da77684a781133d64d3671d23cfca344e
phamnhatkhanh <khanhhcm4@gmail.com>
2022-04-11 14:26:31 +0700

    nsfk

---
+++ index.js
@@ -0,0 +1,18 @@
+const tasks = require("./routes/tasks");
+const connection = require("./db");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const path= require("path")
+connection();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "build")))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,"build","index.html"))
+})
+app.use("/api/tasks", tasks);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
